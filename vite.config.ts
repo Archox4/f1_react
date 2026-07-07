a544f86f5a71f0d0441eb82e9d417bf484022876
Archox4 <123patryk890@gmail.com>
2026-07-07 11:47:10 +0200

    api address to env

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import svgr from 'vite-plugin-svgr';
 
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const apiTarget = env.VITE_API_TARGET || 'http://localhost:8081'
+
   return {
     plugins: [
       react(),
@@ -13,7 +16,7 @@ export default defineConfig(() => {
     server: {
       proxy: {
         '/api': {
-          target: 'http://localhost:8081',
+          target: apiTarget,
           changeOrigin: true,
           secure: false,
         },
